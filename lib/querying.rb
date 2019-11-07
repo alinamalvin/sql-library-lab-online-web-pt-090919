0d6856a74de919d83c94c5b08d9eb2230f24018d
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-07 02:38:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM characters INNER JOIN series on series.id = characters.series_id  limit 1""
 end
 
 def select_character_names_and_number_of_books_they_are_in
